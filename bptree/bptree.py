49cbb535fdeabc62a73088f20dff0653cbd24ca1
Ju Yun Kim <kimj4@carleton.edu>
2016-10-26 15:52:04 -0500

    worked on bptree again

--- bptree/bptree.py
+++ bptree/bptree.py
@@ -39,7 +39,9 @@ class Node():
 
     def is_full(self):
         if (self.is_leaf):
-            if (self.num_pointers == self.capacity):
+            if (self.num_pointers == self.capacity + 1):
+                print self.capacity
+                print "is_full evaluates true"
                 return True
             else:
                 return False
@@ -53,10 +55,12 @@ class Node():
     def insert_key(self, key):
         position_to_insert = 0
         for i in range(len(self.key_list)):
-            if self.key_list[i] < key:
-                position_to_insert = i;
+            if (self.key_list[i] < key):
+                position_to_insert = i + 1;
         self.key_list.insert(position_to_insert, key)
-        self.num_keys = self.num_keys +  1
+        print "position to insert: %s" % position_to_insert
+        self.num_keys = self.num_keys + 1  
+        print "numkeys: %s" % self.num_keys
 
     def insert_pointer(self, pointer):
         position_to_insert = 0
@@ -65,6 +69,7 @@ class Node():
                 position_to_insert = i
         self.pointer_list.insert(position_to_insert, pointer)
         self.num_pointers = self.num_keys + 1
+        print "num pointers: %s" % self.num_pointers
 
     # def is_full(self):
     #     if (self.)
@@ -82,34 +87,43 @@ class Bptree():
     def insert_helper(self, node, key, value):
         # node is leaf
         if (node.is_leaf):
+
             # if node, insert key and value no matter what
             #  check for split case afterwards 
             node.insert_key(key)
+            print node.key_list
             node.insert_pointer(value)
-            print node.capacity
-            print "number of pointers: %s" % node.num_pointers
-            print "number of keys: %s" % node.num_keys
+            # print "before checking if full: %s" % node.key_list
             if node.is_full():
                 print("insert_helper: node is full")
                 if node.is_root:
-                    leftNode = Node(self.n)
-                    leftNode.key_list = node.key_list[0: self.n // 2]
+                    print node.key_list
                     print node.pointer_list
-                    print self.n // 2
+                    leftNode = Node(self.n)
+                    leftNode.is_root = False
+                    leftNode.key_list = node.key_list[: self.n // 2]
+                    # print "length of leftNode key_list: %s" % len(leftNode.key_list)
                     print leftNode.key_list
-                    leftNode.pointer_list = node.pointer_list[0: self.n // 2]
-                    rightNode = Node(self.n)
-                    rightNode.key_list = node.key_list[self.n // 2: -1]
 
-                    rightNode.pointer_list = node.pointer_list[self.n // 2: -1]
+
+                    leftNode.pointer_list = node.pointer_list[  : self.n // 2]
+                    rightNode = Node(self.n)
+                    rightNode.is_root = False
+                    rightNode.key_list = node.key_list[self.n // 2: ]
+                    # print "length of rightNode key_list: %s" % len(rightNode.key_list)
+                    print rightNode.key_list
+
+                    rightNode.pointer_list = node.pointer_list[self.n // 2:]
                     newRoot = Node(self.n)
-                    newRoot.key_list[0] = rightNode.key_list[0]
-                    newRoot.pointer_list[0] = leftNode
-                    newRoot.pointer_list[1] = rightNode
+                    newRoot.is_leaf = False
+                    newRoot.key_list.append(rightNode.key_list[0])
+                    newRoot.pointer_list.append(leftNode)
+                    newRoot.pointer_list.append(rightNode)
                     # special case split
-                    pass
+                    return newRoot
                 else:
                     # typcial case split
+                    print "asdfasdfasdf "
                     pass
             else:
                 # all work is done
@@ -122,7 +136,7 @@ class Bptree():
             for i in range(len(node.key_list)):
                 if key < node.key_list[i]:
                     traversal_idx = i 
-            newEntry = insert_helper(node.pointer_list[traversal_idx], key, value)
+            newEntry = self.insert_helper(node.pointer_list[traversal_idx], key, value)
 
 
 
@@ -149,7 +163,9 @@ class Bptree():
         if self.is_empty:
             self.root = Node(self.n)
             self.is_empty = False
-        self.insert_helper(self.root, key, value)
+        new = self.insert_helper(self.root, key, value)
+        if new:
+            self.root = new
 
 
     def getValueHelper(self, node, key):
@@ -161,10 +177,15 @@ class Bptree():
                 return node.pointer_list[pointer_idx]
         else:
             # pointer_idx = 0;
-            i = 0
-            while (key < node.key_list[i]):
-                i += 1
-            return self.getValueHelper(node.pointer_list[i], key)
+            idx = 0
+            for i in range(len(node.key_list)):
+                if key < node.key_list[i]:
+                    idx = i
+            return self.getValueHelper(node.pointer_list[idx], key)
+            # i = 0
+            # while (key < node.key_list[i]):
+            #     i += 1
+            # return self.getValueHelper(node.pointer_list[i], key)
 
     """Returns the value associated with a particular key.
     Returns None if the key is not in the tree."""
@@ -180,29 +201,47 @@ class Bptree():
     expanding to the right, and indenting further for deeper levels.
     Doing a recursive depth-first traversal of the tree made this pretty easy."""
     def printTree(self):
-        pass
+        print self.printTreeHelper(self.root)
+
+    def printTreeHelper(self, node):
+        if node.is_leaf:
+            return "(%s)" % node.key_list;
+        else:
+            build = ""
+            build = build + str(node.key_list)
+            for item in node.pointer_list:
+                build = build + self.printTreeHelper(item)
+            return build
 
 if __name__ == "__main__":
     # b = bptree.Bptree(4)  # Each node contains 4 keys, which means 5 pointers
-    b = Bptree(4)
+    b = Bptree(3)
     b.insert(0, "a")
     # print "after inserting a:"
     # print b.key_list  
     b.insert(1, "b")
     b.insert(2, "c")
+    # print b.root.key_list
+    # print b.root.pointer_list
+    # print "left:"
+    # print b.root.pointer_list[0].key_list
+    # print "right"
+    # print b.root.pointer_list[1].key_list
     b.insert(3, "d")
-    b.insert(4, "e")
-    b.insert(5, "f")
-    b.insert(6, "g")
-    print b.getValue(0)
-    print b.getValue(1)
-    print b.getValue(2)
-    print b.getValue(3)
-    print b.getValue(4)
-    print b.getValue(5)
-    print b.getValue(6)
-    print b.root.pointer_list   
+
+    # b.insert(4, "e")
+    # b.insert(5, "f")
+    # b.insert(6, "g")
+    # print b.getValue(0)
+    # print b.getValue(1)
+    # print b.getValue(2)
+    # print b.getValue(3)
+    # print b.getValue(4)
+    # print b.getValue(5)
+    # print b.getValue(6)
+    # print b.root.pointer_list   
     # b.printTree()
+    b.printTree()
 
 
 
